e1aa7b7570b4680103a203588e15fb4ff51b2564
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-08-30 21:35:14 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email){
+
+    const body = document.querySelector('body')
+
+    const userData = {
+        name: "Steve",
+        email: "steve@steve.com",
+    }
+
+    const url = "http://localhost:3000/users";
+
+    const object = {
+        method: 'POST',
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json",
+        },
+        body:JSON.stringify(userData)
+    }
+    return(fetch(url,object)
+    .then(res=>res.json())
+    .then(data=>body.textContent=data.id)
+    .catch(error=>{
+        return(body.textContent = error)
+    }))
+}
+
+console.log(submitData(name, email));
